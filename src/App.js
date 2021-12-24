b227ca869ea646c57948f8c3dfc22c571eb0172a
Sid <sidchopp@gmail.com>
2021-12-24 09:05:54 -0700

    Sep comp for left and right side

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+//MUI
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
 
 //Components
-import Mainpage from './components/Mapinpage'
+import HeroImage from './components/HeroImage'
+import SearchImage from './components/SearchImage';
 
+//CSS
 import './App.css';
 
+const theme = createTheme();
 function App() {
   return (
-    <div >
-      <Mainpage />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <HeroImage />
+        <SearchImage />
+      </Grid>
+    </ThemeProvider>
   );
 }
 
