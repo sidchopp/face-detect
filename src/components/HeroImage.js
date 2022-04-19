82906e46e11a92c6e133c53bd577c509ebd9a08d
Sid <sidchopp@gmail.com>
2022-04-19 17:21:01 -0600

    USeStyles included

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,20 +1,16 @@
 import Grid from '@mui/material/Grid';
+//Components
+import UseStyles from './UseStyles';
 
 function HeroImage() {
+  const classes = UseStyles();
   return (
     <Grid
       item
       xs={false}
       sm={4}
       md={7}
-      sx={{
-        backgroundImage: 'url(https://images.unsplash.com/photo-1590255865715-111b097b69d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: (t) =>
-          t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
+      className={classes.image}
     />
   );
 }
