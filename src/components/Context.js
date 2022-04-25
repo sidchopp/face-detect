107f8bb505b7d5fdf6fcf80e093e74fb3b6dd9a0
Sid <sidchopp@gmail.com>
2022-04-25 12:50:10 -0600

    Clarifai package installed

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,6 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react';
 const AppContext = React.createContext();
 
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key YOUR_CLARIFAI_API_KEY");
+
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   return <AppContext.Provider value={{ loading }} >{children}</AppContext.Provider>
